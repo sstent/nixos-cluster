f2f449921f3875d491bb8b67b41c44acb97b6f1b
sstent <stuart.stent@gmail.com>
2023-11-19 21:35:38 +0000

    finish nomad, add consul, sops

--- modules/common.nix
+++ modules/common.nix
@@ -8,25 +8,26 @@
   imports = [
     ./mnt-public.nix
     ./nomad.nix
+    ./consul.nix
     ./odroid-m1-setleds.nix
     ./odroid-m1.nix
-    # inputs.sops-nix.nixosModules.sops
+    inputs.sops-nix.nixosModules.sops
   ];
 
-  # ##secretstore path variable
-  # options._secretstore = lib.mkOption {
-  #   type = lib.types.str;
-  #   default = "${inputs.self}/secrets";
-  #   description = "Path to the Secrets storage";
-  # };
+  ##secretstore path variable
+  options._secretstore = lib.mkOption {
+    type = lib.types.str;
+    default = "${inputs.self}/secrets";
+    description = "Path to the Secrets storage";
+  };
 
   config = {
     system.stateVersion = "23.11"; # Did you read the comment?
 
-    # sops = {
-    #   defaultSopsFile = "${config._secretstore}/host-secrets.yaml";
-    #   age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-    # };
+    sops = {
+      defaultSopsFile = "${config._secretstore}/host-secrets.yaml";
+      age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    };
 
     # Enable nix flakes
     nix.package = pkgs.nixFlakes;
