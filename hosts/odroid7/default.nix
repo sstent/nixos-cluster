657b053b1405767b714cdcf33a059ecbaaf3b631
sstent <stuart.stent@gmail.com>
2023-11-21 19:03:56 +0000

    fix odroid7 and 6

---
+++ hosts/odroid7/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  pkgs,
+  config,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs.hostPlatform.system = "aarch64-linux";
+  networking.hostName = "odroid7";
+  networking.interfaces.end0.ipv4.addresses = [
+    {
+      address = "192.168.1.227";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = ["192.168.1.1" "8.8.8.8"];
+}
