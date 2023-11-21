39078a58d85a78b841649d75bcdf088cc9d6c43e
sstent <stuart.stent@gmail.com>
2023-11-21 00:59:30 +0000

    fix odroid8

--- hosts/odroid8/default.nix
+++ hosts/odroid8/default.nix
@@ -11,4 +11,14 @@
 
   nixpkgs.hostPlatform.system = "aarch64-linux";
   networking.hostName = "odroid8";
+
+    networking.interfaces.end0.ipv4.addresses = [
+    {
+      address = "192.168.1.228";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = ["192.168.1.1" "8.8.8.8"];
 }
