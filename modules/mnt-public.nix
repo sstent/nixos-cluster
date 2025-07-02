721319fa1a06fc8b8f51e1fb9dce706cb0b31afb
sstent <stuart.stent@gmail.com>
2025-07-02 13:15:28 +0000

    sync

--- modules/mnt-public.nix
+++ modules/mnt-public.nix
@@ -6,7 +6,7 @@
   ...
 }: {
   fileSystems."/mnt/Public" = {
-    device = "//192.168.1.109/Public";
+    device = "//192.168.4.109/Public";
     fsType = "cifs";
     # options = ["uid=0,gid=1000"];
     options = ["guest" "uid=1000"];
