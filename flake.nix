b75b899aeadb0aab84880a5b2cb808c39888e519
sstent <stuart.stent@gmail.com>
2026-07-02 19:06:48 -0700

    fix: revert odroid3 and sd-odroid3 to nixpkgs-2311 now that gccarch-armv7-a is configured on opti3

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-2311.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     sops-nix.url = github:Mic92/sops-nix;
   };
@@ -129,7 +130,7 @@
           ./hosts/odroid-hc2-base
         ];
       };
-      odroid3 = nixpkgs.lib.nixosSystem {
+      odroid3 = inputs.nixpkgs-2311.lib.nixosSystem {
         system = "armv7l-linux";
         specialArgs = {inherit inputs;};
         modules = [
@@ -139,11 +140,11 @@
           ./hosts/odroid3
         ];
       };
-      sd-odroid3 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      sd-odroid3 = inputs.nixpkgs-2311.lib.nixosSystem {
+        system = "armv7l-linux";
         specialArgs = {inherit inputs;};
         modules = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-armv7l-multiplatform.nix"
+          "${inputs.nixpkgs-2311}/nixos/modules/installer/sd-card/sd-image-armv7l-multiplatform.nix"
           {
             system.configurationRevision = self.rev or self.dirtyRev or null;
           }
