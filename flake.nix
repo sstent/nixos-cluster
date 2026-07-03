a85b151677b857599fadda006e3adfa19e2fb98e
sstent <stuart.stent@gmail.com>
2026-07-02 18:36:39 -0700

    fix: sd-odroid3 as x86_64 cross-compile, remove minimal-base.nix from image config, drop debug nixpkgs inputs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs-pinned.url = "github:NixOS/nixpkgs/4062d36ebeae843c750011eef6b61ec9a9dbc9a9";
-    nixpkgs-2311.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     sops-nix.url = github:Mic92/sops-nix;
   };
@@ -131,7 +129,7 @@
           ./hosts/odroid-hc2-base
         ];
       };
-      odroid3 = inputs.nixpkgs-2311.lib.nixosSystem {
+      odroid3 = nixpkgs.lib.nixosSystem {
         system = "armv7l-linux";
         specialArgs = {inherit inputs;};
         modules = [
@@ -141,11 +139,11 @@
           ./hosts/odroid3
         ];
       };
-      sd-odroid3 = inputs.nixpkgs-2311.lib.nixosSystem {
-        system = "armv7l-linux";
+      sd-odroid3 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         specialArgs = {inherit inputs;};
         modules = [
-          "${inputs.nixpkgs-2311}/nixos/modules/installer/sd-card/sd-image-armv7l-multiplatform.nix"
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-armv7l-multiplatform.nix"
           {
             system.configurationRevision = self.rev or self.dirtyRev or null;
           }
--- hosts/odroid3/default.nix
+++ hosts/odroid3/default.nix
@@ -1,7 +1,6 @@
 { lib, pkgs, config, inputs, ... }: {
   imports = [
     ../odroid-hc2-base
-    ../../modules/minimal-base.nix
   ];
 
   networking.hostName = lib.mkForce "odroid3";
@@ -10,6 +9,19 @@
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
   };
-  
+
   boot.loader.generic-extlinux-compatible.enable = true;
+
+  # Minimal runtime configuration — no cluster stack
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+  users.users.root = {
+    initialPassword = lib.mkForce "odroid";
+    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwn26AL26A0Yt4sE+rm5//p8QKuNGI/ezAdNJX9QAjRErjEWnsiUr+w0O78912A2RCakdZYZJo6p1RuLYq6u27mjdLU1hhJs1t/ZFUjevKP33Q8hjptnV3s/G/iPfl0h4kQDStNySgJJ7cGh8Dhj906BrQbns3U2WgVZWwhaYvFiSjZA9UWwvB+n/jN9YeSShfdqGYw8/WlFZiOZrz4poO6/DUOAiztvzrpaQFDtI2f9TdGL1ttvYk04jDCRO1cM1LjgWir+WToalgyAqxfgnlvbv8g16RQo//8qhRdMqQPJKnIRewy/VLN1VbNbO2+z5f6BYbYlfioDXmuzMb86jfQ== id_rsa"];
+  };
+
+  nix.extraOptions = "experimental-features = nix-command flakes";
+  environment.systemPackages = [ pkgs.git pkgs.jq ];
 }
